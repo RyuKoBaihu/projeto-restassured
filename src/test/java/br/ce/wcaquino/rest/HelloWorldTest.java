e95ba68ba6728c87ec2c07f49b9e6d216157cace
RyuKoBaihu <yamamoto.araujo.matheus@gmail.com>
2023-05-06 12:53:16 -0300

    AssertEquals

--- src/test/java/br/ce/wcaquino/rest/HelloWorldTest.java
+++ src/test/java/br/ce/wcaquino/rest/HelloWorldTest.java
@@ -12,10 +12,8 @@ public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
         Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
-        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
-        Assert.assertTrue(response.statusCode() == 200);
+        Assert.assertEquals("Ola Mundo!",response.getBody().asString());
+        Assert.assertEquals(200,response.statusCode());
 
-        ValidatableResponse validacao = response.then();
-        validacao.statusCode(200);
     }
 }
