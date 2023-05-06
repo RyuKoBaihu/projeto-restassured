24fe9726d15fe33c97dd30cfe0a325ef0d9e05a4
RyuKoBaihu <yamamoto.araujo.matheus@gmail.com>
2023-05-06 12:41:02 -0300

    assert

--- src/test/java/br/ce/wcaquino/rest/HelloWorld.java
+++ src/test/java/br/ce/wcaquino/rest/HelloWorld.java
@@ -7,11 +7,6 @@ import io.restassured.response.ValidatableResponse;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
-        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
-        System.out.println(response.statusCode() == 200);
 
-        ValidatableResponse validacao = response.then();
-        validacao.statusCode(200);
     }
 }
--- src/test/java/br/ce/wcaquino/rest/HelloWorldTest.java
+++ src/test/java/br/ce/wcaquino/rest/HelloWorldTest.java
@@ -1,5 +1,21 @@
 package br.ce.wcaquino.rest;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HelloWorldTest {
 
+    @Test
+    public void testHelloWorld() {
+        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertTrue(response.statusCode() == 200);
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
 }
