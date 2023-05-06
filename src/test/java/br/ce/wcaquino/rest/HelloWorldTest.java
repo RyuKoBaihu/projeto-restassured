fe28e560b98ab358c8d13a7213a00b95da6afe77
RyuKoBaihu <yamamoto.araujo.matheus@gmail.com>
2023-05-06 13:36:41 -0300

    refactoring methods

--- src/test/java/br/ce/wcaquino/rest/HelloWorldTest.java
+++ src/test/java/br/ce/wcaquino/rest/HelloWorldTest.java
@@ -1,24 +1,33 @@
 package br.ce.wcaquino.rest;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.management.StringValueExp;
+
+import static io.restassured.RestAssured.*;
+
 public class HelloWorldTest {
-    Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+    Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+    Integer statusCodeOK = 200;
 
     @Test
-    public void testHelloWorld() {
+    public void metodoAssertEquals() {
         Assert.assertEquals("Ola Mundo!",response.getBody().asString());
         Assert.assertEquals(200,response.statusCode());
     }
 
     @Test
-    public void testeOutrosMetodos(){
-        Assert.assertEquals("Ola Mundo!",response.getBody().asString());
-        Assert.assertEquals(200,response.statusCode());
+    public void metodoValidatableResponse(){
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+
+    @Test
+    public void metodoGivenWhenThen(){
+        given().when().response().then().statusCode(statusCodeOK);
     }
 }
